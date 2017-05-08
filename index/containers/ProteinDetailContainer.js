5cc749d0d7ce284c8dd7a8afbaa2268cf73a447a
Joseph Romano <jdr2160@cumc.columbia.edu>
2017-05-08 15:56:02 -0400

    BROKEN: NEED TO PULL JSON FROM PROMISE IN index/containers/ProteinDetailContainer.js

--- index/containers/ProteinDetailContainer.js
+++ index/containers/ProteinDetailContainer.js
@@ -1,15 +1,17 @@
 import { connect } from 'react-redux';
 import { removeProtein, updateProtein } from '../actions';
 import ProteinDetail from '../components/ProteinDetail';
+import { getProtein } from '../helpers/api_fetch';
 
-const getProteinFromVkbId = (proteins, id) => {
-    let foundProtein;
-
+const getProteinFromVkbId = (id) => {
+    /*
     proteins.forEach(curProtein => {
         if(curProtein.venomkb_id === id) {
             foundProtein = curProtein;
         }
     }, this);
+    */
+    const foundProtein = getProtein(id);
 
     return foundProtein;
 };
@@ -18,7 +20,9 @@ const mapStateToProps = (state, props) => {
     const { index } = props.params;
 
     // we have the VenomKB id; find the protein that goes with it
-    const singleProtein = getProteinFromVkbId(state.proteins, index);
+    const singleProtein = getProteinFromVkbId(index);
+    // ISSUE OCCURS HERE! NEED TO GET THE JSON OBJECT OUT OF THE PROMISE...
+    // e.g., `singleProtein` is a promise, not a JSON object.
     console.log(singleProtein);
     return {
         protein: singleProtein
