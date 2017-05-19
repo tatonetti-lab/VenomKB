50c2d45e60d7c0af92f7a5c83e2ea4af8d638e83
Joseph Romano <jdr2160@cumc.columbia.edu>
2017-05-19 13:35:10 -0400

    [Hopefully] connected ProteinsContainer to the search functionality.
    Next up: try sticking a form in and hook it up to search
    
    If doesn't work: Probably will have to revert to previous commit

--- index/containers/ProteinsContainer.js
+++ index/containers/ProteinsContainer.js
@@ -1,11 +1,28 @@
 import { connect } from 'react-redux';
 import { addProtein, removeProtein, updateProtein, moveProtein } from '../actions';
+import { createSearchAction, getSearchSelectors } from 'redux-search';
+import { createSelector } from 'reselect';
 import Proteins from '../components/Proteins';
+
+const proteins = state => state.resources.proteins;
+
+const { text, result } = getSearchSelectors({
+    resourceName: 'proteins',
+    resourceSelector: (resourceName, state) => state.resources.proteins
+});
 
 const mapStateToProps = (state) => {
     return {
         proteins: state.resources.proteins,
-        species: state.resources.species
+        species: state.resources.species,
+        selectors: createSelector(
+            [result, proteins, text],
+            (proteinIds, prots, searchText) => ({
+                proteinIds,
+                prots,
+                searchText
+            })
+        )
     };
 };
 
@@ -16,7 +33,8 @@ const mapDispatchToProps = (dispatch) => {
         updateProtein: (id, updates) =>
             dispatch(updateProtein(id, updates)),
         moveProtein: (dragIndex, hoverIndex, protein) =>
-            dispatch(moveProtein(dragIndex, hoverIndex, protein))
+            dispatch(moveProtein(dragIndex, hoverIndex, protein)),
+        searchProtein: createSearchAction('proteins')
     };
 };
 
