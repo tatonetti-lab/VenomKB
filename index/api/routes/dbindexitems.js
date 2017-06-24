a6be703fe9276b42d7dfcb8a10a84280ecd649e5
Joseph Romano <jdromano2@gmail.com>
2017-06-24 16:57:55 +0000

    Fix capitalization issue in api code

--- index/api/routes/dbindexitems.js
+++ index/api/routes/dbindexitems.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const dbindexitem = require('../models/dbindexitem.js');
+const dbindexitem = require('../models/Dbindexitem.js');
 
 router.get('/', (req, res, next) => {
     dbindexitem.find({}).exec((err, dbindexitems) => {
