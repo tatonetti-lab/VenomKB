6a03e8b5bc091b22b75002cb2e259efda9359457
Joseph Romano <JDRomano2@gmail.com>
2017-06-25 16:00:53 -0400

    Reduce bundle size by turning off source map

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
+    // devtool: 'eval-source-map',
     entry: [
         'babel-polyfill',
         'webpack-dev-server/client?http://localhost:3000',
@@ -34,7 +34,12 @@ module.exports = {
           'process.env.VENOMKB_STAGING_PW': process.env.VENOMKB_STAGING_PW
         }),
         new ExtractTextPlugin('styles.css'),
-        new UglifyJSPlugin()
+        new UglifyJSPlugin({
+            compressor: {
+                warnings: false,
+                screw_ie8: true
+            }
+        })
     ],
     module: {
         rules: [
