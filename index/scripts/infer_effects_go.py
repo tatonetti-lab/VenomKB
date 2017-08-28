2ad856a1e409139d115c92eea3671f029e606fc0
Joseph Romano <JDRomano2@gmail.com>
2017-08-28 17:29:16 -0400

    Finish working copy of graph generation script.
    I'm not convinced that the current setup is most efficient, but it
    should work for importing into Neo4j, and for running most classical
    graph algorithms.

--- index/scripts/infer_effects_go.py
+++ index/scripts/infer_effects_go.py
@@ -8,7 +8,10 @@ import json
 import ipdb
 import pickle
 from tqdm import tqdm
+from lxml import etree
 from collections import defaultdict
+from StringIO import StringIO
+import ipdb
 
 from venomkb_builder import VenomKB
 
@@ -40,13 +43,45 @@ class Graph(object):
     def has_key(self, key):
         return key in self._graph.keys()
 
-    def add_node(self, label, parents=[], children=[]):
-        assert set(parents).issubset(self._graph.keys())
-        assert set(children).issubset(self._graph.keys())
-        assert label not in self._graph.keys()
-        self._graph[label] = children
-        for p in parents:
-            self._graph[p].append(label)
+    # def add_node(self, label, parents=[], c=[]):
+    #     print(label)
+    #     print(parents)
+    #     print(c)
+    #     try:
+    #         assert set(parents).issubset(self._graph.keys())
+    #         assert set(c).issubset(self._graph.keys())
+    #         assert label not in self._graph.keys()
+    #         self._graph[label] = c
+    #         for p in parents:
+    #             self._graph[p].append(label)
+    #     except AssertionError:
+    #         ipdb.set_trace()
+    #         print("Uh oh!")
+
+    def add_edge(self, parent, child):
+        if parent in self._graph.keys():
+            self._graph[parent].append(child)
+        else:
+            self._graph[parent] = [child]
+        # Make sure child node exists. If not, create it
+        if not child in self._graph.keys():
+            #if child == 'GO:0044216':
+            #    ipdb.set_trace()
+            #self.add_node(label=child, parents)
+            self._graph[child] = []
+
+    def count_edges(self):
+        """Self-explanatory - uses double list comprehension"""
+        return len([l for sl in [x for x in g._graph.values()] for i in sl])
+
+    def remove_node(self, label):
+        # Remove the node
+        if not self._graph.pop(label, False):
+            return False
+        # Remove all edges pointing to the node
+        for k, v in self._graph.iterkeys():
+            self._graph[k] = [x for x in v if x != label]
+        return True
 
     def find_path(self, start, end, path=[]):
         """You should usually use find_shortest_path(),
@@ -98,11 +133,11 @@ class Graph(object):
         
         This isn't an efficient algorithm!
 
-        Complexity: O(n^2)"""
+        Complexity: at least O(n^2)"""
         candidates = self._graph.keys()
         roots = []
         for c in candidates:
-            if len([1 for _, v in testgraph.iteritems() if c in v]) > 0:
+            if c not in [i for sl in self._graph.values() for i in sl]:
                 roots.append(c)
         if len(roots) == 0:
             return None
@@ -112,19 +147,83 @@ class Graph(object):
         """Create a list of nodes where each node occurs before all
         of its children.
 
-        Solution is not guaranteed to be unique."""
+        Solution is not guaranteed to be unique, but all DAGs have at
+        least one solution.
+
+        For algorithm details refer to doi:10.1145/368996.369025"""
         L = []
         Q = self.find_all_roots()
-        
+        graph_copy = dict(self._graph)
+        while len(Q) > 0:
+            n = Q.pop()
+            L.append(n)
+            for m in graph_copy[n]:
+                graph_copy[n] = [x for x in graph_copy[n] if x != m]
+                if len([1 for k, v in graph_copy.iteritems() if m in v]) > 0:
+                    Q.append(m)
+        if bool([x for x in graph_copy.values() if x != []]):
+            return False
+        else:
+            return L
 
     def is_dag(self):
         """If we can perform topological sort, then it is a DAG."""
-        pass
+        if self.topological_sort():
+            return True
+        else:
+            return False
 
 
 class GOgraph(Graph):
     def __init__(self):
         super(GOgraph, self).__init__()
+        self._rel_table = defaultdict(dict)
+        self.already_added = []
+
+    quickgo_base = 'http://www.ebi.ac.uk/QuickGO-Old/'
+
+    def add_term_with_ancestors(self, go_id):
+        if go_id in self.already_added:
+            return False
+        obo = GOgraph.fetch_quickgo_by_term_id(go_id)
+        relations = GOgraph.parse_quickgo_oboxml(obo)
+        for x in relations:
+            self.add_edge(go_id, x['id'])
+            self._rel_table[go_id][x['id']] = x['type']
+            if x['id'] not in self.already_added:
+                self.add_term_with_ancestors(x['id'])
+        self.already_added.append(go_id)
+        return relations
+
+    @classmethod
+    def fetch_quickgo_by_term_id(cls, id):
+        """Sample ID: GO:0003824"""
+        url_suffix = 'GTerm?id={0}&format=oboxml'.format(id)
+        r = requests.get(GOgraph.quickgo_base + url_suffix)
+        oboxml = r.text
+        et = etree.parse(StringIO(oboxml))
+        return et
+
+    @classmethod
+    def parse_quickgo_oboxml(cls, quickgo_xml):
+        is_a = quickgo_xml.xpath('/obo/term/is_a')
+        relationships = quickgo_xml.xpath('/obo/term/relationship')
+        is_a_terms = [x.text.strip() for x in is_a]
+        terms = []
+        for rt in relationships:
+            reltype = rt.xpath('type')[0].text.strip()
+            relid = rt.xpath('to')[0].text.strip()
+            terms.append({'type': reltype,
+                          'id': relid})
+        for i in is_a_terms:
+            terms.append({'type': 'is_a',
+                          'id': i})
+        return terms
+
+    @classmethod
+    def reltolabel(cls, relation):
+        return "{0};{1}".format(relation['type'], relation['id'])
+
 
 
 go_by_prot = []
@@ -147,3 +246,8 @@ else:
     with open('go_annotations.pkl', 'rb') as fp:
         go_by_prot = pickle.load(fp)
 
+g = GOgraph()
+for gbp in tqdm(go_by_prot):
+    for go in gbp['go_annotations']:
+        if not str(go['id']) in g.already_added:
+            g.add_term_with_ancestors(str(go['id']))
