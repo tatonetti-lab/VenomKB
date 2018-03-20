559326aae1f27fbdd4c8021ae714541a126a3ce8
Joseph Romano <JDRomano2@gmail.com>
2018-03-20 10:47:49 -0400

    Build out better ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,52 +1,75 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
+    /**
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps : [
 
-    // First application
-    {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
-    },
+        {
+            name      : 'VenomKB API',
+            script    : './index/bin/www',
+            env: {
+                MONGO_ADDR: 'mongodb://34.197.121.158:27017'
+            },
+            env_production : {
+                NODE_ENV: 'production',
+                API_HOSTNAME: 'http://venomkb.org/api/'
+            },
+            env_test : {
+                NODE_ENV: 'development',
+                API_HOSTNAME: 'http://54.165.86.64/api/'
+            },
+            env_dev : {
+                NODE_ENV: 'development'
+            }
+        },
+        {
+            name : 'VenomKB',
+            script : './server/index.js',
+            env_test : {
+                NODE_ENV: 'test',
+                API_HOSTNAME: 'http://54.165.86.64/api/'
+            },
+            env_production : {
+                NODE_ENV: 'production',
+                API_HOSTNAME: 'http://venomkb.org/api/'
+            },
+            env_dev : {
+                NODE_ENV: 'development',
+                API_HOSTNAME: 'http://localhost:3001/api/'
+            }
+        }
+    ],
 
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
+    /**
+     * http://pm2.keymetrics.io/docs/usage/deployment/
+     */
+    deploy : {
+        production : {
+            user : 'ubuntu',
+            host : 'venomkb.org',
+            ref  : 'origin/master',
+            repo : 'git@github.com:JDRomano2/venomkb.git',
+            path : '/var/www/venomkb',
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+        },
+        dev : {
+            user : 'jdr2160',
+            host : 'localhost',
+            ref  : 'origin/master',
+            repo : 'git@github.com:JDRomano2/venomkb.git',
+            path : '/User/jdr2160/developer/venomkb',
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+            env  : {
+                NODE_ENV: 'development'
+            }
+        },
+        test : {
+            user : 'ubuntu',
+            host : '54.165.86.64',
+            ref  : 'origin/master',
+            repo : 'git@github.com:JDRomano2/venomkb.git',
+            path : '/var/www/venomkb',
+            'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env test'
+        }
     }
-  ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
 };
